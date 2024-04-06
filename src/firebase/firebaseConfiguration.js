cc41f213a50be6810ff0fe9981be66d3b1e7a558
Rahul <rahultaakhr@gmail.com>
2024-04-06 16:17:49 +0530

    dene

--- src/firebase/firebaseConfiguration.js
+++ src/firebase/firebaseConfiguration.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
     // messagingSenderId:(import.meta.env.VITE_FIREBASE_MESSAGINGSENDER),
     // appId: (import.meta.env.VITE_FIREBASE_APPID),
     // measurementId:(import.meta.env.VITE_FIREBASE_MEASUREMENTID),
-    apiKey:String(import.meta.env.VITE_FIREBASE_APIKEY),
+    apiKey:String(import.meta.env.VITE_APIKEY),
     authDomain: "ecommerce-6dfad.firebaseapp.com",
     projectId: "ecommerce-6dfad",
     storageBucket: "ecommerce-6dfad.appspot.com",
