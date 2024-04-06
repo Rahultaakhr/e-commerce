31eacf6befb464d9c1f4900115ec437f0a62b6d6
Rahul <rahultaakhr@gmail.com>
2024-04-06 16:45:29 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
