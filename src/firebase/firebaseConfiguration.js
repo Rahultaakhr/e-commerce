937d9b7b74a41dc272e134bad1dfd36004949a45
Rahul <rahultaakhr@gmail.com>
2024-04-06 16:14:45 +0530

    done

--- src/firebase/firebaseConfiguration.js
+++ src/firebase/firebaseConfiguration.js
@@ -3,17 +3,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey:String(import.meta.env.VITE_FIREBASE_APIKEY),
-    authDomain: (import.meta.env.VITE_FIREBASE_AUTHDOMAIN),
-    projectId: (import.meta.env.VITE_FIREBASE_PROJECTID),
-    storageBucket: (import.meta.env.VITE_FIREBASE_STORAGEBUCKET),
-    messagingSenderId:(import.meta.env.VITE_FIREBASE_MESSAGINGSENDER),
-    appId: (import.meta.env.VITE_FIREBASE_APPID),
-    measurementId:(import.meta.env.VITE_FIREBASE_MEASUREMENTID),
+    // apiKey:String(import.meta.env.VITE_FIREBASE_APIKEY),
+    // authDomain: (import.meta.env.VITE_FIREBASE_AUTHDOMAIN),
+    // projectId: (import.meta.env.VITE_FIREBASE_PROJECTID),
+    // storageBucket: (import.meta.env.VITE_FIREBASE_STORAGEBUCKET),
+    // messagingSenderId:(import.meta.env.VITE_FIREBASE_MESSAGINGSENDER),
+    // appId: (import.meta.env.VITE_FIREBASE_APPID),
+    // measurementId:(import.meta.env.VITE_FIREBASE_MEASUREMENTID),
+    apiKey: "AIzaSyA7uSSuZguG-z_K6CYx6voKHku_MJImetw",
+    authDomain: "ecommerce-6dfad.firebaseapp.com",
+    projectId: "ecommerce-6dfad",
+    storageBucket: "ecommerce-6dfad.appspot.com",
+    messagingSenderId: "767839234541",
+    appId: "1:767839234541:web:fe42f5fda0eadff4199fe4",
+    measurementId: "G-MKE2VEGZ8P"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const fireDb = getFirestore(app)
 const auth = getAuth(app)
-export {fireDb,auth,firebaseConfig}
+export { fireDb, auth, firebaseConfig }
